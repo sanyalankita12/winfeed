bb752b015a45f4c2a5edc675775525307847c6df
sanyalankita12 <sanyal.ankita12@gmail.com>
2026-05-19 11:05:54 +0530

    Add pagination to home feed

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,8 @@ main=Blueprint('main',__name__)
 @main.route('/')
 @main.route('/home')
 def home():
-    posts=Post.query.order_by(Post.date_posted.desc()).all()
+    page=request.args.get('page',1,type=int)
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
     return render_template('home.html',posts=posts)
 
 @main.route('/register',methods=['GET','POST'])
