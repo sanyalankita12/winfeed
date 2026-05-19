7fcd6ec6e5b5d36d1ef6984cf2814ae5b8dd504e
sanyalankita12 <sanyal.ankita12@gmail.com>
2026-05-19 10:44:26 +0530

    Add custom 404 and 500 error pages

--- app/routes.py
+++ app/routes.py
@@ -122,4 +122,11 @@ def react_post(post_id,emoji):
         db.session.commit()
     return redirect(url_for('main.home'))
 
+@main.app_errorhandler(404)
+def error_404(error):
+    return render_template('404.html'),404
+
+@main.app_errorhandler(500)
+def error_500(error):
+    return render_template('500.html'),500
 
