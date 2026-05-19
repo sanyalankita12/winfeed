75f8f24736faa370f200de47ff143848f120f813
sanyalankita12 <sanyal.ankita12@gmail.com>
2026-05-19 11:51:15 +0530

    Fix gunicorn port binding for Railway

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-for-csrf'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey123'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///winfeed_1.db'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
